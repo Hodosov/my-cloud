a9a310e4004923eb69496e59513581b1e1021e45
Hodosov Yaroslav <hodosov.yar@gruzf.ru>
2021-07-10 15:09:36 +0300

    add auth routs

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const config = require('config')
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const authRouter = require("./routes/auth.routes");
+const app = express();
+const PORT = config.get("serverPort");
 
-const app = express()
-const PORT = config.get('serverPort')
-const start =async () => {
-    try {
-        await mongoose.connect(config.get("dbUrl"), { useNewUrlParser: true, useUnifiedTopology: true } )
-        app.listen(PORT, () => {
-            console.log(`server start on PORT:${PORT}`)
-        })
-    } catch (error) {
-        console.warn('error', error)
-    }
-}
+app.use(express.json())
+app.use("/api/auth", authRouter);
+const start = async () => {
+  try {
+    await mongoose.connect(config.get("dbUrl"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`server start on PORT:${PORT}`);
+    });
+  } catch (error) {
+    console.warn("error", error);
+  }
+};
 
-start()
+start();

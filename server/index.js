2316235d73a4e74d72921945de56477b1a7877e2
Hodosov Yaroslav <hodosov.yar@gruzf.ru>
2021-07-10 14:11:43 +0300

    connect to mongo db

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log('server run')
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+const PORT = config.get('serverPort')
+const start =async () => {
+    try {
+        await mongoose.connect(config.get("dbUrl"), { useNewUrlParser: true, useUnifiedTopology: true } )
+        app.listen(PORT, () => {
+            console.log(`server start on PORT:${PORT}`)
+        })
+    } catch (error) {
+        console.warn('error', error)
+    }
+}
+
+start()

448f72dcb04b78a79372691e40c234f0fec2fc03
Hodosov Yaroslav <hodosov.yar@gruzf.ru>
2021-07-15 19:25:38 +0300

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import AppBar from "../src/components/Header/AppBar";
 import { RegistrationForm } from "./components/RegistrationForm";
 import { makeStyles } from "@material-ui/core/styles";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { LoginForm } from "./components/LoginForm copy";
+import { LoginForm } from "./components/LoginForm";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { auth } from "./actions/user";
@@ -17,11 +17,11 @@ function App() {
   const classes = useStyles();
 
   const isAuth = useSelector((state) => state.user.isAuth);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(auth())
-  }, [])
+    dispatch(auth());
+  }, []);
 
   return (
     <BrowserRouter>
